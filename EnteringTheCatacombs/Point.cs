c85231b2fc3a0d11d40fd76119c7f543bedf3b99
Bear <bearfulmer@gmail.com>
2026-03-13 09:03:51 -0700

    Add project files.

---
+++ EnteringTheCatacombs/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnteringTheCatacombs
+{
+    internal class Point
+    {
+        public int x;
+        public int y;
+        
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Point() 
+        { 
+            x=0; y=0;  // Default constructor initializes to (0, 0)
+        }
+
+        // Override ToString for easy debugging and display
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
---
+++ EnteringTheCatacombs/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EnteringTheCatacombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = new Point[2];
+            Console.Title = "Entering the Catacombs";
+            Console.WriteLine("Entering the Catacombs!");
+
+            points[0] = new Point(2, 3);
+            points[1] = new Point(-4, 0);
+
+            Console.WriteLine($"Point 1: {points[0]}");
+            Console.WriteLine($"Point 2: {points[1]}");
+        }
+    }
+}
