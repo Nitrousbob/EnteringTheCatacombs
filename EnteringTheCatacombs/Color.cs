8e9ae357a88cf8397a3f2875d53e1623c80b88d7
BearFulmer <bearfulmer@gmail.com>
2026-03-19 11:17:55 -0700

    Created a Card Class and a Color Class, Built the Deck in the Program and Displays the color and type of card in the Deck.

---
+++ EnteringTheCatacombs/Color.cs
@@ -0,0 +1,30 @@
+﻿namespace EnteringTheCatacombs
+{
+    internal class TheColor
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+
+        public TheColor(byte r, byte g, byte b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+        public static TheColor White => new TheColor(255, 255, 255);
+        public static TheColor Black => new TheColor(0, 0, 0);
+        public static TheColor Red => new TheColor(255, 0, 0);
+        public static TheColor Orange => new TheColor(255, 165, 0);
+        public static TheColor Yellow => new TheColor(255, 255, 0);
+        public static TheColor Green => new TheColor(0, 128, 0);
+        public static TheColor Blue => new TheColor(0, 0, 255);
+        public static TheColor Purple => new TheColor(128, 0, 128);
+
+        public override string ToString()
+        {
+            return $"R:{R} G:{G} B:{B}";
+        }
+    }
+}
--- EnteringTheCatacombs/Program.cs
+++ EnteringTheCatacombs/Program.cs
@@ -13,6 +13,57 @@
 
             Console.WriteLine($"Point 1: {points[0]}");
             Console.WriteLine($"Point 2: {points[1]}");
+
+            TheColor hotColor = new(200, 200, 0);
+            Console.WriteLine($"HotColor {hotColor}");
+            TheColor coldColor = TheColor.Blue;
+            Console.WriteLine($"ColdColor {coldColor}");
+
+            Card goodCard = new(Card.CardColor.Red, Card.Rank.One);
+            Console.WriteLine($"GoodCard {goodCard} a {goodCard.GetCardType()}");
+
+            Card awesomeCard = new(Card.CardColor.Green, Card.Rank.Dollar);
+            Console.WriteLine($"AwesomeCard {awesomeCard} a {awesomeCard.GetCardType()}");
+            
+            List<Card> deck = CreateDeck();
+
+            foreach (Card card in deck)
+            {
+                Console.WriteLine($"{card} a {card.GetCardType()}");
+            }
+        }
+
+        public static List<Card> CreateDeck()
+        {
+            List<Card> deck = new List<Card>();
+            Card.CardColor color;
+
+            for (int i = 0; i < 4; i++)  //Make a loop for all colors
+            {
+                switch (i)  //change the card color each iteration of i
+                {
+                    case 0:
+                        color = Card.CardColor.Red;
+                        break;
+                    case 1:
+                        color = Card.CardColor.Green;
+                        break;
+                    case 2:
+                        color = Card.CardColor.Blue;
+                        break;
+                    default:
+                        color = Card.CardColor.Yellow;
+                        break;
+                }
+
+                for (int j = 0; j < 14; j++) //make a loop for all cards within each color
+                {
+                    Card.Rank rank = (Card.Rank)j;
+                    Card card = new Card(color, rank);  //create the card with the color and rank
+                    deck.Add(card);  //add the card to the deck
+                }
+            }
+            return deck;
         }
     }
 }
